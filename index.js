145c93b2b0ab31a8ea1b9a1e1309667294b25da6
Akinniyi Tolulope <47461817+Joshuafreemant@users.noreply.github.com>
2022-12-06 00:05:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,10 @@ app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 app.use(morgan('common'));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: 'https://breeds.onrender.com/',
+    origin: 'http://localhost:3000/'
+}));
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')));//set the directory of where we store files
 
 // FILE STORAGE
