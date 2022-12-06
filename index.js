7d9105b638a97a32d21ff1c0d0ac808e83b28634
Akinniyi Tolulope <47461817+Joshuafreemant@users.noreply.github.com>
2022-12-06 10:05:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -41,10 +41,7 @@ app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 app.use(morgan('common'));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors({
-    origin: 'https://breeds.onrender.com/',
-    origin: 'http://localhost:3000/'
-}));
+
 
 app.use(cors({
     origin: ['https://breeds.onrender.com/', 'https://breeds.onrender.com','http://localhost:3000/','http://localhost:3000']
